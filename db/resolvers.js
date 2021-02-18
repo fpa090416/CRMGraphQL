0ca9c4dba7b66f495821149b975913be23947ab6
Everth Paredes <fpa090416@gmail.com>
2021-02-17 22:22:34 -0600

    Se agrego funcionalidad para agregar nuevos usuarios y la autenticacion de los mismos

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,7 +1,64 @@
+const Usuario = require('../models/Usuario');
+
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
+
+const crearToken = (usuario, secreta, expiresIn) =>{
+    //console.log(usuario);
+    const { id, email, nombre, apellido } = usuario;
+
+    return jwt.sign( { id, email, nombre, apellido }, secreta, { expiresIn } )
+}
+
+
 //Resolver
 const resolvers = {
     Query: {
-        obtenerAlgo: ()=>"Algo"
+        obtenerCurso: ()=>"Algo"
+    },
+    Mutation:{
+        nuevoUsuario: async (_,{input})=>{
+            const { email, password } = input;
+
+            //Revisando si el usuario existe
+            const existeUsuario = await Usuario.findOne({email});
+            if(existeUsuario){
+                throw new Error('El usuario ya existe')
+            }
+
+            //Hashear el password
+            const salt = await bcryptjs.genSalt(10);
+            input.password = await bcryptjs.hash(password, salt);
+
+            try{
+                //Guardar en la base de datos
+                const usuario = new Usuario(input);
+                usuario.save() //Guardar en la base de datos
+                return usuario;
+            } catch(error){
+                console.log(error);
+            }
+        },
+        autenticarUsuario: async(_,{input})=>{
+            const { email, password } = input;
+
+            //Verificar si el usuario existe
+            const existeUsuario = await Usuario.findOne({email});
+            if(!existeUsuario){
+                throw new Error(`El usuario ${email} no existe en la base de datos`)
+            }
+
+            //Revisar si el password es correcto
+            const passwordCorrecto = await bcryptjs.compare( password, existeUsuario.password );
+            if(!passwordCorrecto){
+                throw new Error('El password es Incorrecto');
+            }
+            //Crear el token
+            return {
+                token: crearToken(existeUsuario, process.env.SECRETA, '24h')
+            }
+        }
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -2,8 +2,46 @@ const { gql } = require('apollo-server');
 
 //Schema
 const typeDefs = gql`
+    ##Types's
+    type Usuario{
+        id: ID
+        nombre: String
+        apellido: String
+        email: String
+        creado: String
+    }
+    # type Login{
+    #     emal: String
+    #     fecha: String
+    # }
+    type Token{
+        token: String
+    }
+
+    ##Inputs
+    input UsuarioInput{
+        nombre: String!
+        apellido: String!
+        email:String!
+        password: String!
+    }
+    input AutenticarInput{
+        email: String!
+        password: String!
+    }
+    # input LoginInput{
+    #     email: String!
+    # }
+
+    ##Funciones
     type Query{
-        obtenerAlgo: String
+        obtenerCurso: String
+    }
+
+    type Mutation{
+        ##Usuario
+        nuevoUsuario(input: UsuarioInput): Usuario
+        autenticarUsuario(input: AutenticarInput): Token
     }
 `;
 
